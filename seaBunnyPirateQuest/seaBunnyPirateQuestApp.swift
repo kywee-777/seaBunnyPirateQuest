8e0073df24f9b2f97d1ca3993dcdbd9c9b3ac679
kywee777 <nguyenm.kylie@gmail.com>
2023-06-22 12:11:24 -0700

    Initial Commit

---
+++ seaBunnyPirateQuest/seaBunnyPirateQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  seaBunnyPirateQuestApp.swift
+//  seaBunnyPirateQuest
+//
+//  Created by Kylie Nguyen on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct seaBunnyPirateQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
